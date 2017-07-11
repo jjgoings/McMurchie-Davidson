dd35ccfe1e6ed675dcb15ee3674eb34f81bda6a5
Joshua Goings <jjgoings@gmail.com>
2017-07-11 14:45:31 -0700

    add scf tests

---
+++ tests/test_scf.py
@@ -0,0 +1,21 @@
+import unittest
+import numpy as np
+from mmd.integrals import *
+from mmd.molecule import *
+from mmd.scf import *
+
+class test_SCF(unittest.TestCase):
+    def test_water(self):
+        geom = './geoms/test/h2o.dat'
+        mol = Molecule(filename=geom,basis='sto-3g')
+        mol.build()
+        scf = SCF(mol)
+        scf.RHF()
+        self.assertAlmostEqual(scf.mol.energy.real,-74.942079928192)
+    def test_methane(self):
+        geom = './geoms/test/ch4.dat'
+        mol = Molecule(filename=geom,basis='sto-3g')
+        mol.build()
+        scf = SCF(mol)
+        scf.RHF()
+        self.assertAlmostEqual(scf.mol.energy.real,-39.726850324347)
