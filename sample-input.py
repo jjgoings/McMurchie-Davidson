74d2461095287519a6d61e9661c7835efe1dadd0
Joshua Goings <jjgoings@gmail.com>
2017-08-03 10:02:10 -0700

    change basis of sample

--- sample-input.py
+++ sample-input.py
@@ -10,7 +10,7 @@ H   -0.866811829    0.601435779    0.000000
 """
 
 # init molecule and build integrals
-mol = Molecule(geometry=water,basis='DZ')
+mol = Molecule(geometry=water,basis='sto-3g')
 
 # do the SCF
 mol.RHF()
