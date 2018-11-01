c86f93aa2e904be16194af911ff3b7bf2b4a0c0f
Joshua Goings <jjgoings@gmail.com>
2018-11-01 19:00:21 -0400

    add codecov

---
+++ tests/test_integrals.py
@@ -0,0 +1,8 @@
+import unittest
+import numpy as np
+from mmd.integrals.onee import _boys as boys 
+
+class test_integrals(unittest.TestCase):
+    def test_overlap(self):
+        pass
+        #self.assertAlmostEqual(boys(0.5,6.8),7.34475165333247E-02,places=places)
