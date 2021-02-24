9777f04e4784d5074b47c4918a7a3b590bf43e70
jjgoings <jjgoings@gmail.com>
2021-02-24 15:38:49 -0800

    add standalone davidson test w/ near-degeneracies

--- mmd/utils/davidson.py
+++ mmd/utils/davidson.py
@@ -69,32 +69,3 @@ def davidson(A,roots,tol=1e-8):
     if converged:
         return E, X 
 
-if __name__ == '__main__':
-
-    import time
-    
-    t0 = time.time()
-    np.random.seed(0)
-    dim = 1000
-    A = np.diag(np.arange(dim,dtype=np.float64))
-    A[1:3,1:3] = 0
-    M = np.random.randn(dim,dim)
-    M += M.T
-    A += 1e-4*M
-    
-    t1 = time.time()
-    print("Matrix formation (s): %.2f" % float(t1-t0))
-    
-    roots = 3
-    E, C = davidson(A, roots)
-    t2 = time.time()
-    print("Davidson (s):         %.2f" % float(t2-t1))
-    
-    E_true, C_true = np.linalg.eigh(A)
-    E_true, C_true = E_true[:roots], C_true[:,:roots]
-    t3 = time.time()
-    print("Numpy (s):            %.2f" % float(t3-t2))
-    
-    assert np.allclose(E, E_true)
-    print("Eigs:                 ", E)
-
