fb7985e2fdd7ec6fb07d54194998e235eb06057b
jjgoings <jjgoings@gmail.com>
2021-02-23 20:33:25 -0800

    add naive davidson restart

--- mmd/utils/davidson.py
+++ mmd/utils/davidson.py
@@ -1,7 +1,8 @@
 import numpy as np
 import sys
 
-def davidson(A,roots,tol=1e-6):
+def davidson(A,roots,tol=1e-8):
+    # FIXME: currently does poor job when degenerate roots are involved
 
     mat_dim = A.shape[0]
     sub_dim = 4*roots
@@ -17,9 +18,9 @@ def davidson(A,roots,tol=1e-6):
         E,C = E[:roots], C[:,:roots]
          
         # get current approx. eigenvectors
-        X = V @ C    
+        X = V @ C   
 
-        # calc residuals
+        # form residual vectors 
         R = np.zeros((mat_dim,roots))
         Delta = np.zeros((mat_dim,roots))
         for j in range(roots):
@@ -29,7 +30,7 @@ def davidson(A,roots,tol=1e-6):
                 if np.abs(E[j] - A[i,i]) < 1e-4:
                     continue # don't let preconditioner blow up -- keep as zero
                 else:
-                    preconditioner[i] = 1/(E[j] - A[i,i])
+                    preconditioner[i] = -1/(A[i,i] - E[j])
 
             # normalize correction vectors
             Delta[:,j] = preconditioner * R[:,j]
@@ -40,17 +41,25 @@ def davidson(A,roots,tol=1e-6):
             q = (np.eye(mat_dim)  - V @ V.T) @ Delta[:,j]
             norm = np.linalg.norm(q)
 
-            if (norm > 1e-3):
-                if (sub_dim + 1 > mat_dim):
-                    sys.exit("sub_dim > mat_dim")
+            if (norm > 1e-4):
+                if (sub_dim + 1 > min(500,mat_dim//4)):
+                    # lazy subspace collapse 
+                    # FIXME: need to figure out better restart
+                    print("Subspace too big: collapsing")
+                    print("Eigs at current: ", E)
+                    sub_dim = roots
+                    V = X
+                    V, _ = np.linalg.qr(V)
+                    break
+                    
+                else:
+                    V_copy = np.copy(V)
+                    sub_dim += 1
+                    V = np.eye(mat_dim,sub_dim) 
+                    V[:,:(sub_dim-1)] = V_copy
+                    # add new vector to end of subspace
+                    V[:,-1] = q/norm
 
-                V_copy = np.copy(V)
-                sub_dim += 1
-                V = np.eye(mat_dim,sub_dim) 
-                V[:,:(sub_dim-1)] = V_copy
-                # add new vector to end of subspace
-                V[:,-1] = q/norm
-       
         # check convergence
         converged = True
         for j in range(roots):
@@ -58,5 +67,5 @@ def davidson(A,roots,tol=1e-6):
                 converged = False     
 
     if converged:
-        return E, C
+        return E, X 
 
